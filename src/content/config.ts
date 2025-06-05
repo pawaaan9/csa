66cfb3cb05086198c833b50bef6a2ab029a74ae0
pawaaan9 <pawankanchana99@gmail.com>
2025-06-05 13:08:13 +0530

    new

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,3 +1,21 @@
+// src/content/config.ts
+import { defineCollection, z } from "astro:content";
+
+const articlesCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string().optional(),
+    pubDate: z.coerce.date(),
+    author: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export default {
+  articles: articlesCollection,
+};
+
 export const bookingLink =
   "https://outlook.office365.com/book/GetYourQuestionsAnswered@criticalsa.com/";
 
